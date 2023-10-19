63b0e8e7a338e0c524559aab0ea882c5faf2b40c
Shah Hussain <shhahhussain@gmail.com>
2023-10-19 15:47:44 +0500

    initial commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const titleController = require("./titleController");
 const app = express();
 
 app.get("/I/want/title/", titleController.getWebsiteTitles);
-
+app.all("*", (req, res) => {
+    res.status(404).send("Not Found");
+  });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
